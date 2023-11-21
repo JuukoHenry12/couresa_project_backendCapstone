ab5461b168d765ad29ed3e859bbd5393cbfc63ba
juuko henry <40009917+JuukoHenry12@users.noreply.github.com>
2023-11-21 23:13:16 +0300

    update
    update

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 from rest_framework.routers import DefaultRouter
+from djoser.views import UserViewSet
 
 router: DefaultRouter = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
@@ -15,5 +16,10 @@ urlpatterns = [
     # API paths
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
     path('booking/', include(router.urls)),
-    path('registration/',views.RegistrationView.as_view(),name='registration')
+    path('registration/', views.RegistrationView.as_view(), name='registration'),
+    path('api/users/',
+         UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
+    path('api/users/<int:pk>/', UserViewSet.as_view(
+        {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='user-detail'),
+    path('api/users/me/', UserViewSet.as_view({'get': 'me'}), name='user-me'),
 ]
